a53d2afb69debc1bf69ff61a9e32250ba5819181
cremonamusic <cremonastrings@gmail.com>
2026-06-20 15:44:01 +0800

    Add files via upload

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,4 +1,4 @@
-﻿// Firebase Configuration - I TALK Teachers
+﻿// Firebase Configuration - Cremona Music Portal
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
